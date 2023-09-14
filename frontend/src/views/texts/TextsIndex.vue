7613b6f249537adbab2c617f92a72365567a2e05
Masashi Kawakami <dlold1962@gmail.com>
2023-09-14 23:32:30 +0900

    Add updated by to text table

--- frontend/src/views/texts/TextsIndex.vue
+++ frontend/src/views/texts/TextsIndex.vue
@@ -214,6 +214,7 @@
               <th scope="col" class="px-6 py-3">Tags</th>
               <th scope="col" class="px-6 py-3">Created at</th>
               <th scope="col" class="px-6 py-3">Updated at</th>
+              <th scope="col" class="px-6 py-3">Updated by</th>
             </tr>
           </thead>
           <tbody>
@@ -254,6 +255,17 @@
               </td>
               <td class="px-3 py-2">
                 {{ formatDateToYyyyMmDdHhMmSs(fetchedText.updatedAt) }}
+              </td>
+              <td class="px-3 py-2">
+                <div
+                  class="cursor-pointer flex flex-col space-y-4 items-center justify-center"
+                >
+                  <img
+                    class="rounded-full w-12"
+                    src="default_icon2.svg"
+                    alt="image description"
+                  />
+                </div>
               </td>
             </tr>
           </tbody>
