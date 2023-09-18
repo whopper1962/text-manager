23fcbb55b419d903bc5b900f931337db91dfd9b3
Masashi Kawakami <dlold1962@gmail.com>
2023-09-18 09:13:26 +0900

    Add total data count to pagination component

--- frontend/src/components/AppPagniation.vue
+++ frontend/src/components/AppPagniation.vue
@@ -86,6 +86,13 @@
         </li>
       </ul>
     </nav>
+    <span class="text-sm font-normal text-gray-500 dark:text-gray-400"
+      >Showing
+      <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of
+      <span class="font-semibold text-gray-900 dark:text-white"
+        >1000</span
+      ></span
+    >
   </div>
 </template>
 
