16bf0b2febc991c8c55af527b1cb0627afdc877b
Masashi Kawakami <dlold1962@gmail.com>
2023-09-20 23:47:50 +0900

    Add popper to updater icon

--- frontend/src/views/texts/TextsIndex.vue
+++ frontend/src/views/texts/TextsIndex.vue
@@ -261,10 +261,21 @@
                   class="cursor-pointer flex flex-col space-y-4 items-center justify-center"
                 >
                   <img
+                    :data-popover-target="`popover-${fetchedText.id}`"
                     class="rounded-full w-12"
                     src="default_icon2.svg"
                     alt="image description"
                   />
+                </div>
+                <div
+                  data-popover
+                  :id="`popover-${fetchedText.id}`"
+                  role="tooltip"
+                  class="absolute z-10 invisible inline-block w-fit text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
+                >
+                  <div class="px-3 py-2">
+                    <p>whopper1962</p>
+                  </div>
                 </div>
               </td>
             </tr>
